85f8f86a2924297ede11ea1abae23237047d9ef2
Ikillnukes <ikillnukes1@gmail.com>
2018-08-27 21:10:56 +0200

    ApiURL

--- js/api.js
+++ js/api.js
@@ -1,5 +1,5 @@
 function _getAction(el, action, fn) {
-    $.get('api/api.php', { action: action, resources: 'https://raw.githubusercontent.com/z3nth10n/public_portafolio/master/json/resources.json' }, function(data) { fn(el, data); });
+    $.get(apiUrl, { action: action, resources: 'https://raw.githubusercontent.com/z3nth10n/public_portafolio/master/json/resources.json' }, function(data) { fn(el, data); });
 }
 
 _getAction($("section[data-action='getportafolio']"), 'get-css', function(el, data) {
