e26a4435ed18965000f69b48270d9525aac18ca3
Ikillnukes <ikillnukes1@gmail.com>
2018-08-27 20:22:42 +0200

    I forgot elements

--- js/api.js
+++ js/api.js
@@ -2,10 +2,10 @@ function getAction(el, action, fn) {
     $.get('api/api.php', { action: action, resources: 'https://raw.githubusercontent.com/z3nth10n/public_portafolio/master/json/resources.json' }, function(data) { fn(el, data); });
 }
 
-getAction('get-css', function(el, data) {
+getAction($("section[data-action='getportafolio']"), 'get-css', function(el, data) {
     el.append('<link>'+data+'</link>');
 });
 
-getAction('get-js', function(el, data) {
+getAction($("section[data-action='getportafolio']"), 'get-js', function(el, data) {
     el.append('<script>'+data+'</script>');
 });
