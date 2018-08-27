2f4a416d3452f6f23d099c17076cb1315328b536
Ikillnukes <ikillnukes1@gmail.com>
2018-08-27 20:41:29 +0200

    Little fixes

--- js/api.js
+++ js/api.js
@@ -1,11 +1,11 @@
-function getAction(el, action, fn) {
+function _getAction(el, action, fn) {
     $.get('api/api.php', { action: action, resources: 'https://raw.githubusercontent.com/z3nth10n/public_portafolio/master/json/resources.json' }, function(data) { fn(el, data); });
 }
 
-getAction($("section[data-action='getportafolio']"), 'get-css', function(el, data) {
-    el.append('<link>'+data+'</link>');
+_getAction($("section[data-action='getportafolio']"), 'get-css', function(el, data) {
+    el.append('<style>'+data+'</style>');
 });
 
-getAction($("section[data-action='getportafolio']"), 'get-js', function(el, data) {
-    el.append('<script>'+data+'</script>');
+_getAction(null, 'get-js', function(el, data) {
+    eval(data);
 });
